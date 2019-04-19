d95a2a0bb77ec29206005055ddceef25a258ce57
Tenyson05 <Rojahog@hotmail.com>
2019-04-19 05:42:09 -0700

    Updated UI scroll

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'homepage.dart';
 
 void main() => runApp(Netflix());
 
@@ -6,51 +7,46 @@ class Netflix extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Netflix",
+      title: 'Flutter Demo',
       theme: ThemeData(
         primaryColor: Colors.black,
-        primaryColorBrightness: Brightness.light,
       ),
       home: NetflixTabBar(),
     );
   }
 }
-
-
-
 class NetflixTabBar extends StatelessWidget {
- 
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 4,
-      child: Theme(
-        data: ThemeData(
-          brightness: Brightness.dark,
-        ),
-        child: Scaffold(
+        length: 4,
+        child: Theme(
+          data: ThemeData(
+            brightness: Brightness.dark
+          ),
+          child: Scaffold(
           bottomNavigationBar: TabBar(
-            tabs: <Widget>[
-              Tab(icon: Icon(Icons.home),text: "Home",),
-              Tab(icon: Icon(Icons.search),text: "Search",),
-              Tab(icon: Icon(Icons.file_download),text: "Downlaod",),
-              Tab(icon: Icon(Icons.list),text: "More",),
+            tabs: [
+                Tab(icon: Icon(Icons.home,), text: "Home",),
+                Tab(icon: Icon(Icons.search), text: "Search"),
+                Tab(icon: Icon(Icons.file_download), text: "Downloads"),
+                Tab(icon: Icon(Icons.list), text: "More"),
               ],
-            unselectedLabelColor: Colors.white,
-            labelColor: Colors.black,
-            indicatorColor: Colors.red,
+            unselectedLabelColor: Color(0xff999999),
+            labelColor: Colors.white,
+            indicatorColor: Colors.transparent
           ),
           body: TabBarView(
-            children: <Widget>[
-              Center( child: Text("Home")),
-              Center( child: Text("Search")),
-              Center( child: Text("Downlaod")),
-              Center( child: Text("More")),
+            children: [
+              HomePage(),
+              Center( child: Text("Page 2")),
+              Center( child: Text("Page 3"),),
+              Center( child: Text("Page 4")),
             ],
           ),
         ),
-      ),
-    );  
+        )
+      );
   }
 }
-
